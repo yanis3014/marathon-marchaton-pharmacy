542d7b787b25e4e4caa6e6e0f11aa2faacdcb608
yanis3014 <146885811+yanis3014@users.noreply.github.com>
2025-10-22 10:52:56 +0200

    correction chemin image

--- frontend/app/(site)/page.jsx
+++ frontend/app/(site)/page.jsx
@@ -89,22 +89,22 @@ export default function Page() {
         <div className="flex justify-center items-center gap-8 md:gap-16 flex-wrap">
           {/* Remplace les src par les chemins réels vers tes logos */}
           <img
-            src="/images/sponsors/sponsor1.jpg"
+            src="/sponsors/sponsor1.png"
             alt="Sponsor 1"
             className="h-16 max-w-[150px] object-contain"
           />
           <img
-            src="/images/sponsors/sponsor2.jpg"
+            src="/sponsors/sponsor2.jpg"
             alt="Sponsor 2"
             className="h-16 max-w-[150px] object-contain"
           />
           <img
-            src="/images/sponsors/sponsor3.jpg"
+            src="/sponsors/sponsor3.jpg"
             alt="Sponsor 3"
             className="h-16 max-w-[150px] object-contain"
           />
           <img
-            src="/images/sponsors/sponsor4.png"
+            src="/sponsors/sponsor4.png"
             alt="Sponsor 1"
             className="h-16 max-w-[150px] object-contain"
           />
